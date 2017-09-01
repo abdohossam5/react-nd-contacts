913ae02b5be56896981378356b664a8cd3465288
abdomokhtar <abdel@incredible-qr.com>
2017-09-01 17:40:19 +0800

    - Lesson 2-4: Add contacts list component.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+class ContactsList extends Component {
+    render() {
+        const contacts = this.props.contacts;
+        return (
+            <ol>
+                {contacts.map( c => <li key={c.name}>{c.name}</li> )}
+            </ol>
+        )
+    }
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <ContactsList contacts={[
+                    {name:'Abdo'},
+                    {name:'John'}
+                ]}/>
+                <ContactsList contacts={[
+                    {name:'Adam'},
+                    {name:'Arya'}
+                ]}/>
+            </div>
+        );
+    }
 }
 
 export default App;
