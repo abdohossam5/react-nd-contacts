5e13a61e1617c7ab2988fe85ad1831e9ae292548
abdomokhtar <abdel@incredible-qr.com>
2017-09-04 01:47:53 +0800

    - create contact form init.

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput'
 
 class CreateContact extends Component {
 
     render() {
         return (
             <div>
-                Create Contact Working
+                <Link to="/" className="close-create-contact"/>
+
+                <form className="create-contact-form">
+                    <ImageInput
+                        className="create-contact-avatar-input"
+                        name="avatar"
+                        maxHeight={80}
+                    />
+
+                    <div className="create-contact-details">
+                        <input name="name" placeholder="Name"/>
+                        <input name="email" placeholder="Email"/>
+                    </div>
+                </form>
             </div>
         )
     }
